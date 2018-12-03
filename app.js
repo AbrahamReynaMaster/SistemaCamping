577818825d3dcddf2215020998574e5d18edd2c9
Abraham Reyna <apolo561@hotmail.com>
2018-12-03 15:31:16 -0600

    new

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Make our db accessible to our router
 app.use(function(req,res,next){
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('../public/index.html', { title: 'Holi' });
+  res.render('index.html', { title: 'Home' });
+});
+
+router.get('/adduser',function (req,res,next){
+  res.json('addUser.html',{title: 'Add User'});
 });
 
 router.get('/getUsersCamping',function(req,res,next){
